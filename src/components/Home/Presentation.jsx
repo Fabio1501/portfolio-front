169210655e8aa44b32ca626698095ddb316a11d5
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-22 16:07:44 -0300

    feat(home): create presentation home

---
+++ src/components/Home/Presentation.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Presentation = ({}) => {
+
+    return (
+        <div className=''>
+            <div key >
+
+            </div> 
+        </div>
+    );
+}
+
+export default Presentation
