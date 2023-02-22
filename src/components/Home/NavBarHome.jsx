7f7de11718ae9a55f4d1c1b7b0e22c69dab732cb
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-22 16:07:24 -0300

    feat(home): create component navbarHome

---
+++ src/components/Home/NavBarHome.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const NavBarHome = ({}) => {
+
+    return (
+        <div className=''>
+            <div key >
+
+            </div> 
+        </div>
+    );
+}
+
+export default NavBarHome
