687b284b60b259d89b70a6de8a31f59d12d8841f
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-24 09:52:41 -0300

    feat(curriculum): create file

---
+++ src/components/Curriculum/Curriculum.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Curriculum = () => {
+
+    return (
+        <section className='w-80 bg-zinc-700/60 p-8 mt-16 mb-16 rounded-lg shadow-[0_20px_25px_-14px_rgba(0,0,0,0.5)]'>
+            <p className='text-white text-sm'>Curriculum</p>
+        </section>
+    );
+}
+
+export default Curriculum;
