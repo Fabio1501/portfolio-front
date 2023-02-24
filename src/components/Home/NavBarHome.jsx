fd5e6be4f7730e13d41eb5cc6a8031676d79e222
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-24 09:55:08 -0300

    feat(navBarHome): create component

--- src/components/Home/NavBarHome.jsx
+++ src/components/Home/NavBarHome.jsx
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
-const NavBarHome = ({}) => {
+const NavBarHome = ({infoNav}) => {
 
     return (
-        <div className=''>
-            <div key >
-
-            </div> 
+        <div className='w-80 bg-zinc-700/60 p-8 mr-32 rounded-lg shadow-[0_20px_25px_-14px_rgba(0,0,0,0.5)]'>
+            <div className='m-auto w-36'>
+                {
+                    <Link to={infoNav.logo.url.href}>
+                        <img src={infoNav.logo.image.src}/>
+                    </Link>
+                }
+            </div>
+            <nav className='mb-16 mt-16'>
+                {infoNav.secciones.map(seccion => {
+                    return (
+                        <Link className='flex items-center justify-start mb-6' to={seccion.url.href}>
+                            <img className='w-7 mr-12' src={seccion.image.src}/>
+                            <div className='text-white text-center text-lg hover:text-[#3BFE0B]'>
+                                {seccion.text.name}
+                            </div>
+                        </Link>
+                    )
+                })}   
+            </nav>
+            <div className='flex items-center justify-evenly'>
+                {
+                    infoNav.redes.map(red => {
+                        return(
+                            <a className='w-9 hover:scale-125' href={red.url.href} target = {red.url.target}>
+                                <img className='w-9' src={red.image.src} alt = {red.image.alt}/>
+                            </a>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
