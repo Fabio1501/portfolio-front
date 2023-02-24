ce01d83cf6e6610b3656c3defa480ebe1b15741e
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-24 09:51:32 -0300

    feat(config): add custom properties

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first-color': '#3BFE0B',
+      },
+      boxShadow: {
+        '3-xl' : '0 20px 25px -14px rgba(0, 0, 0, 0.5)'
+      }
+    },
   },
   plugins: [],
 }
