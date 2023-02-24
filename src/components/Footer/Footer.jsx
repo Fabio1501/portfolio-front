0f8ee59cbcdc678c92eae4b130a66acf2964e41e
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-24 09:52:16 -0300

    feat(footer): create file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Footer = () => {
+
+    return (
+        <footer className='w-full bg-zinc-700/60 p-8 mt-16 mb-16 rounded-lg'>
+           asda
+        </footer>
+    );
+}
+
+export default Footer;
