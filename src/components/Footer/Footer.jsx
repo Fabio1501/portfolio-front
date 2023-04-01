0600e3a1fd760b3c535fc86827728bbfa7372b61
Fabian Carabajal <99187738+Fabio1501@users.noreply.github.com>
2023-04-01 10:48:17 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,26 +21,26 @@ const Footer = () => {
                         <div class="flex items-center justify-evenly">
                             <Link href={staticComponents.footer.logo.url.href} class="flex items-center mb-4 sm:mb-0 hover:cursor-pointer">
                                 <img src={staticComponents.footer.logo.image.src} className="mr-8 h-10" alt={staticComponents.footer.logo.image.alt} />
-                                <div className='flex flex-col justify-center'>
-                                    <span className=" text-l font-semibold whitespace-nowrap dark:text-white">{staticComponents.footer.textLogo.nombre.content}</span>
-                                    <span className="text-l font-normal whitespace-nowrap dark:text-white">{staticComponents.footer.textLogo.derechos.content}</span>
-                                </div>
                             </Link>
+                            <div className='flex flex-col justify-center'>
+                                <span className=" text-l font-semibold whitespace-nowrap dark:text-white">{staticComponents.footer.textLogo.nombre.content}</span>
+                                <span className="text-l font-normal whitespace-nowrap dark:text-white">{staticComponents.footer.textLogo.derechos.content}</span>
+                            </div>
                             <ul class="flex flex-wrap items-center ml-24 mb-6 sm:mb-0">
                                 {
                                     staticComponents.footer.sections.map((section, index) => {
                                         if (index % 2 !== 0) {
                                             return(
                                                 <li>
-                                                <a href="#" className="pl-6 pr-6 text-l text-white hover:underline md:pr-6 md:pl-6 border-x-2">{section.text.content}</a>
-                                            </li>
+                                                    <a href={section.url.href} className="pl-6 pr-6 text-l text-white hover:underline md:pr-6 md:pl-6 border-x-2">{section.text.content}</a>
+                                                </li>
                                             )
                                         }
 
                                         return(
                                         <li>
-                                        <a href="#" className="ml-6 text-l text-white hover:underline md:mr-6">{section.text.content}</a>
-                                    </li>
+                                            <a href={section.url.href} className="ml-6 text-l text-white hover:underline md:mr-6">{section.text.content}</a>
+                                        </li>
                                     )})
                                 }
                             </ul>
